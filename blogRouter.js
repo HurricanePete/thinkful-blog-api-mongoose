db481cca5bf5cf99dcd5e8da1c629abad9a69db9
HurricanePete <zpete725@gmail.com>
2017-08-17 12:05:31 -0400

    add of multiple endpoints
    -add of POST endpoint
    -add of PUT/:id endpoint
    -add of DELETE/:id endpoint

--- blogRouter.js
+++ blogRouter.js
@@ -39,8 +39,7 @@ router.post('/', jsonParser, (req, res) => {
 			console.error(message);
 			return res.status(400).send(message);
 		}
-	}
-
+	};
 	Blogs
 		.create({
 			title: req.body.title,
@@ -51,44 +50,59 @@ router.post('/', jsonParser, (req, res) => {
 			},
 			created: Date.now()
 		})
-		.then(
-			blogs => res.status(201).json(blogs.blogsRepr()))
-		.catch(err=> { 
+		.then(blogs => res.status(200).json(blogs.blogsRepr()))
+		.catch(err => { 
 			console.error(err);
 			res.status(500).json({message: 'Internal server error'});
 		});
 });
 
-router.delete('/:id', (req, res) => {
-	BlogPosts.delete(req.params.id);
-	console.log(`Deleted blog post with ID: \`${req.params.id}\``);
-	res.status(204).end();
-})
-
 router.put('/:id', jsonParser, (req, res) => {
-	const requiredFields = ['title', 'content', 'author'];
-	for (let i=0; i<requiredFields.length; i++) {
-		const field = requiredFields[i];
-		if (!(field in req.body)) {
-			const message = `Missing \`${field}\` in request body`;
-			console.error(message);
-			return res.status(400).send(message);
-		}
-	}
+	if (!('id' in req.body)) {
+		const message = `Please include the id to be updated`;
+		console.error(message);
+		res.status(400).send(message);
+	};
 	if (req.params.id !== req.body.id) {
 		const message = `The reqested id \`${req.params.id}\` does not match the update request for id \`${req.body.id}\``;
 		console.error(message);
 		return res.status(400).send(message);
-	}
+	};
 	console.log(`Updating blog ID: \`${req.params.id}\``);
-	BlogPosts.update({
-		id: req.params.id,
-		name: req.body.name,
-		content: req.body.content,
-		author: req.body.author,
-		publishDate: req.body.publishDate
-	});
-	res.status(204).end();
+
+	const toUpdate = {};
+  	const updateableFields = ['title', 'content', 'author'];
+  	//how can I keep a value for author if I only update firstName or lastName, but not both?
+
+ 	updateableFields.forEach(field => {
+   		if (field in req.body) {
+      	toUpdate[field] = req.body[field];
+    	}
+  	});
+ 	Blogs
+ 		.findByIdAndUpdate(req.params.id, {$set: toUpdate})
+ 		.exec()
+ 		.then(blogs => res.status(200).json(blogs.blogsRepr()))
+ 		.catch(err => {
+ 			console.error(err);
+ 			res.status(500).json({message: 'Internal server error'});
+ 		});
 });
+
+router.delete('/:id', (req, res) => {
+	Blogs
+		.findByIdAndRemove(req.params.id)
+		.exec()
+		.then(blogs => {
+			console.log(`Deleted blog post with ID: \`${req.params.id}\``);
+			res.status(204).end();
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: 'Internal server error'});
+		})
+});
+
+
 
 module.exports = router;
